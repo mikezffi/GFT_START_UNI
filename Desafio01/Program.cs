575f5838e5926ee552f0c23d0161a601f839f00a
mikezffi <michael.zuffi@gmail.com>
2021-02-11 14:20:53 -0300

    Create Desafio01

---
+++ Desafio01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input Multplicando
+
+            //Input Intervalo
+
+            //Check de Erro
+
+            //Calculo
+
+            //Outbut resultado
+        }
+    }
+}
